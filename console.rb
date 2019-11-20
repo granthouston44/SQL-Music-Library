7b10f93e26cd23e50b246cc1ed23e4ce130a76f6
Grant Houston <grant.houston44@hotmail.com>
2019-11-20 16:24:32 +0000

    added artist methods: create, save, all, update, delete and started find by id

--- console.rb
+++ console.rb
@@ -1,34 +1,34 @@
 require('pry')
 require_relative('models/artists')
-require_relative('models/albumbs')
+require_relative('models/albums')
 
-Albums.delete_all()
+# Albums.delete_all()
 Artist.delete_all()
 
 artist1 = Artist.new({
   'name' => 'blink-182'
   })
 
-artist2 = Artist.new({
-  'name' = 'AC/DC'
-  })
-
-artist1.save()
-artist2.save()
-
-album1 = Album.new({
-  'name' => 'Dude Ranch'
-  'genre' => 'Pop-punk'
-  'artist_id' => artist1.id
-  })
-
-  album1.save()
-
-  album2 = Album.new({
-    'name' => 'Back in Black'
-    'genre' => 'Hard Rock'
-    'artist_id' => artist2.id
+  artist2 = Artist.new({
+    'name' =>'AC/DC'
     })
 
-    binding.pry
-    nil
+    artist1.save()
+    artist2.save()
+
+    # album1 = Album.new({
+    #   'name' => 'Dude Ranch',
+    #   'genre' => 'Pop-punk',
+    #   'artist_id' => artist1.id
+    #   })
+
+      # album1.save()
+      #
+      # album2 = Album.new({
+      #   'name' => 'Back in Black',
+      #   'genre' => 'Hard Rock',
+      #   'artist_id' => artist2.id
+      #   })
+
+        binding.pry
+        nil
--- models/artists.rb
+++ models/artists.rb
@@ -1,3 +1,5 @@
+require_relative('../db/SqlRunner')
+
 class Artist
 
 attr_accessor :name
@@ -9,9 +11,35 @@ attr_reader :id
     @name = artist_details['name']
   end
 
+  def save()
+    sql = "INSERT INTO artists
+    (name) VALUES ($1) RETURNING id"
+    values = [@name]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]['id'].to_i
+  end
 
+  def Artist.delete_all
+    sql = "DELETE FROM artists"
+    SqlRunner.run(sql)
+  end
 
+  def Artist.all()
+    sql = "SELECT * FROM artists"
+    artists = SqlRunner.run(sql)
+    return artists.map{|artist| Artist.new(artist)}
+  end
 
+  def update()
+    sql = "UPDATE artists SET name = $1 WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
 
+  def delete_artist
+    sql = "DELETE FROM artists WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
 
 end
